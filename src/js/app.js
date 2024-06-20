7a847c04d159af909bd495adac94487db3ca5bf5
Akio <aakocherga@mail.ru>
2024-06-21 00:39:29 +0700

    [fix] new colors and resizing

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,15 @@
 const btn = document.querySelector(".add-book")
 const menu = document.querySelector(".menu")
-
+const menuContainer = document.querySelector(".menu-container")
+const btnSumbit = document.querySelector(".sumbit")
 
 btn.addEventListener('click', function () {
     menu.style.display = 'flex';
-    document.body.appendChild(menu)
+    document.body.appendChild(menu);
 })
 
-menu.addEventListener('click', function () {
-    menu.style.display = 'none';
-    document.body.appendChild(menu)
+menu.addEventListener('click', function (event) {
+    if (!menuContainer.contains(event.target)) {
+        document.body.removeChild(menu);
+    }
 })
