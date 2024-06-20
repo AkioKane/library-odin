25e0015c64a8199f3e3f31074e2396036efdf2b2
Akio <aakocherga@mail.ru>
2024-06-20 12:34:40 +0700

    [Add] Added reaction btn on click

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
 const btn = document.querySelector(".add-book")
+const menu = document.querySelector(".menu")
+
 
 btn.addEventListener('click', function () {
-    
+    menu.style.display = 'flex';
+    document.body.appendChild(menu)
+})
+
+menu.addEventListener('click', function () {
+    menu.style.display = 'none';
+    document.body.appendChild(menu)
 })
